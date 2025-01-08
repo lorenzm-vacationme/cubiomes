5d0ec1e7ac69536a6a1db4b0148a5a4be1ad1f38
lorenzm-vacationme <lorenz@vacationme.net>
2025-01-08 15:54:24 +0800

    modify file path

--- generate_tiles.c
+++ generate_tiles.c
@@ -144,7 +144,7 @@ int main(int argc, char *argv[]) {
     int tileSize = 96;  // This can be adjusted based on zoom level or as needed
 
     char outputDir[2048];
-    snprintf(outputDir, sizeof(outputDir), "/var/www/production/gme-backend/storage/app/public/tiles");
+    snprintf(outputDir, sizeof(outputDir), "/var/www/staging/gme-backend/storage/app/public/tiles");
 
     if (createDir(outputDir) != 0) {
         return 1;
