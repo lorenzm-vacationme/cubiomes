4f9d9da6fd08db3bcb5d95ab49f8bcd3aef333ad
lorenzm-vacationme <lorenz@vacationme.net>
2025-02-12 17:02:20 +0800

    update directory

--- newLogic.c
+++ newLogic.c
@@ -100,7 +100,8 @@ int main(int argc, char *argv[])
 
     // Define output directory with seed first
     char outputDir[200];
-    snprintf(outputDir, sizeof(outputDir), "/var/www/storage/app/public/tiles/%d/%d/%d", seed, zoom, x);
+    snprintf(outputDir, sizeof(outputDir), "/var/www/production/gme-backend/storage/app/public/tiles/%d/%d/%d", seed, zoom, x);
+    // snprintf(outputDir, sizeof(outputDir), "/var/www/production/gme-backend/storage/app/public/tiles");
 
     // Create directories recursively
     createDirectories(outputDir);
